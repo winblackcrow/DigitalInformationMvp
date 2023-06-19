4fdd9737e8cddfe965a7e09de5c41aea097d38e9
winblackcrow <winblackcrow@gmail.com>
2023-06-20 00:49:03 +0900

    Create-React-App

---
+++ didmvp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-backgroud">
+      <img src={logo}></img>
+      <p>Let's start Digital Information System</p>
+    </div>
+  );
+}
+
+export default App;
